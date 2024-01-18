e21dffc45dd4c8691a5b9feca56e0720783f32a5
Martin Castro Bildhjerd <148054287+MartinBildhjerd@users.noreply.github.com>
2024-01-18 15:15:26 +0100

    Update Attiny.c

--- Attiny.c
+++ Attiny.c
@@ -1,3 +1,4 @@
+
 #define F_CPU 8000000UL
 #define BAUD_RATE 9600
 #include <avr/io.h>
@@ -7,37 +8,34 @@
 #include <xc.h>
 #include <avr/interrupt.h>
 
-void init_serial()
-void init_Attiny85_PORTS()
 unsigned char DataOut = 0;
 
-
-ISR(USART_UDRE_vect) 
+void init_serial()
 {
-	UDR0 = DataOut; 
+	UCSR0B = (1 << TXEN0) | (1 << UDRIE0); // EN transmitter data register empty interrupt.
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // Select 8-bit size.
+	UBRR0L = 0x67; // Config baud rate
+	sei(); // Activate global interrupt system
+}
+
+void init_Attiny85_PORTS()
+{
+	PORTB = (1 << PB0) | (1 << PB2); // input pins
+	DDRB |= (1 << DDB1) | (1 << DDB5); // output pins
+}
+
+ISR(USART_UDRE_vect) // Execute when data register is empty.
+{
+	UDR0 = DataOut; // Sending the data out
 }
 
 int main(void)
 {
-	init_Attiny85_PORTS(); 
+	init_Attiny85_PORTS();
 	init_serial();
-	
-    while (1) 
-    {
+
+	for(;;)
+	{
 		
-    }
-}
-
-void init_serial()
-{
-		UCSR0B = (1<<TXEN0) | (1<<UDRIE0); //EN transmitter data register empty interrupt. 
-		UCSROC = (1<<UCSZ01) | (1<<UCSZ00); //Select 8bit size. 
-		UBR0L = 0x67; //Config baud rate
-		sei //Activate global interrupt system
-}
-
-void init_Attiny85_PORTS()
-{
-	PORTB = (1<<PB0)|(1<<PB2); //input pins
-	DDRB = (1<<DDB1)|(1<<DDB5); //outputpins
+	}
 }
