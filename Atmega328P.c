446540ad8f0cdf800cad208a11f9467c9f8760d5
Martin Castro Bildhjerd <148054287+MartinBildhjerd@users.noreply.github.com>
2024-01-19 12:56:07 +0100

    Update Atmega328P.c

--- Atmega328P.c
+++ Atmega328P.c
@@ -1,41 +1,59 @@
-#define F_CPU 8000000UL
-#define BAUD_RATE 9600
 #include <avr/io.h>
 #include <util/delay.h>
-#include <string.h>
-#include <stdio.h>
-#include <xc.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>
 
-unsigned DataIn;
+#define BAUD_RATE 9600
+#define F_CPU 8000000UL
 
-void init_serial()
+unsigned char ObjectID;
+
+void init_serial() 
 {
 	UCSR0B = (1 << RXEN0) | (1 << RXCIE0);
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-	UBRR0L = 0x67;
+	UBRR0L = 0x33; //9600 BAUDRATE
 	sei();
 }
 
-void init_Atmega328P_PORTS()
+void init_Ports() 
 {
-	DDRB &= ~(1 << PB0); // PB0 in
-	DDRB |= (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4) | (1 << PB5); // PB1-5 out
-	PORTB = (1 << PB0);
+	DDRB &= ~(1 << PB0); //PB0 in
+	DDRB |= (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4) | (1 << PB5); //PB1-5 out
+	PORTB = (1 << PB0); //328p latch 
+	DDRC |= (1<<PC0)|(1 << PC6); //PC0, PC6 out
+	
 }
 
-ISR(USART_RX_vect)
+ISR(USART_RX_vect) 
 {
-	DataIn = UDR0;
-}
-
-int main(void)
-{
-	init_Atmega328P_PORTS();
-	init_serial();
-
-	for(;;)
+	ObjectID = UDR0;
+	if (ObjectID >= 0 && ObjectID <= 255) 
 	{
-		// Your main loop code here
+		switch (ObjectID) 
+		{
+			case 1: //Detection
+			PORTB |= (1 << PB1); 
+			break;
+			case 2: 
+			PORTB |= (1 << PB2); 
+			break;
+			default:
+			break;
+		}
+		} else {
 	}
 }
+
+int main(void) 
+{
+	init_serial();
+	init_Ports();
+
+	for (;;) {
+		
+		//_delay_ms(1000); 
+	}
+	return 0;
+}
+
